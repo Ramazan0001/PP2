83080b646c120772e484336b93b488eb8dba041c
Ramazan0001 <alimbekramazan93@gmail.com>
2026-01-30 15:54:29 +0500

    in prog

---
+++ Practice1/numbers.py
@@ -0,0 +1,37 @@
+#1
+x = 1
+y = 2.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+#2
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+
+#3
+x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+
+#4
+
+x = float(1)
+y = int(2.8)
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
