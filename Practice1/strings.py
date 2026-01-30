89c5f4aba3f7d4b1e75756c1e66d064f640ac2e8
Ramazan0001 <alimbekramazan93@gmail.com>
2026-01-30 16:03:27 +0500

    in prog

---
+++ Practice1/strings.py
@@ -0,0 +1,21 @@
+#1
+b = "Hello, World!"
+print(b[2:5])
+
+#2
+b = "Hello, World!"
+print(b[:5])
+
+#3
+b = "Hello, World!"
+print(b[-5:-2])
+
+#4
+a = "Hello, World!"
+print(a.lower())
+
+#5
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
