76c611d0500aad9978c85326c67e708156908915
Ramazan0001 <alimbekramazan93@gmail.com>
2026-01-30 15:47:40 +0500

    in prog

---
+++ Practice1/data types.py
@@ -0,0 +1,24 @@
+#1
+x = complex(1j)
+print(x)
+print(type(x)) 
+
+#2
+x = range(3, 10)
+print(x)
+print(list(x))
+
+#3
+x = float(20.5)
+print(x)
+print(type(x)) 
+
+#4
+x = tuple(("apple", "banana", "cherry"))
+print(x)
+print(type(x)) 
+
+#5
+x = bool(5)
+print(x)
+print(type(x)) 
