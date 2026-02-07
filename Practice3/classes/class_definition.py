308ce0369352a3947bf8fe0fa9aea8b5161971a1
Ramazan0001 <alimbekramazan93@gmail.com>
2026-02-07 15:19:16 +0500

    in Prog

---
+++ Practice3/classes/class_definition.py
@@ -0,0 +1,22 @@
+#1
+class MyClass:
+  x = 5
+
+print(MyClass)
+#2
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+#3
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
